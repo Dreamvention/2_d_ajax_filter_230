a9a1ea50c3186134fbef1b2922a1beab2d7119df
Alexandr Krasny <php4.drvn@gmail.com>
2017-06-02 14:46:07 +0300

    Initial commit

---
+++ admin/language/english/extension/d_ajax_filter/cache.php
@@ -0,0 +1,6 @@
+<?php
+
+$_['heading_title_main']         = 'Ajax Filter';
+$_['text_form']                  = 'Create cache';
+
+$_['text_installation_progress'] = 'Installation in progress, please wait...';
